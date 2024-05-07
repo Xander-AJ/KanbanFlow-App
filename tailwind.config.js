0c164f1b015bf79bb3204661cd87b0a635f8d209
John-Alexander Kamau <xander.kamau7@gmail.com>
2024-05-07 08:58:03 +0300

    configure content to use tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
+const config = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  mode: "jit",
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
 
+export default config;
