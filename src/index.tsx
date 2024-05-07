4fdfe430c55ab304b57469954f14b6adbf13e23c
John-Alexander Kamau <xander.kamau7@gmail.com>
2024-05-07 10:14:12 +0300

    initialize the React application and render it to the DOM (Document Object Model) of the web page

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { StyleSheetManager } from "styled-components";
+import App from "./App";
+import "./index.css";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<Suspense fallback={<div>Loading...</div>}>
+		<BrowserRouter>
+			<StyleSheetManager shouldForwardProp={(prop) => prop !== "shake"}>
+				<App />
+			</StyleSheetManager>
+		</BrowserRouter>
+	</Suspense>
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
