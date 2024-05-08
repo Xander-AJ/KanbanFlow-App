c4c647e47788fe91c184ee8887b0097ccac33702
Amariah <amariah.abish@gmail.com>
2024-05-08 21:32:22 +0300

    added search, tasks and extra pages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,28 @@
 import { RouteObject } from "react-router";
 import Layout from "../layout";
 import Boards from "../pages/Boards";
+import Home from "../pages/home";
+import Projects from "../pages/projects";
 
 const routes: RouteObject[] = [
-	{
-		path: "/",
-		element: <Layout />,
-		children: [
-			{
-				children: [
-					{
-						path: "",
-						element: <Boards />,
-					},
-				],
-			},
-		],
-	},
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/", // Updated path for Home component
+        element: <Home />,
+      },
+      {
+        path: "/projects", // Updated path for Projects component
+        element: <Projects />,
+      },
+      {
+        path: "/boards", // Updated path for Boards component
+        element: <Boards />,
+      },
+    ],
+  },
 ];
 
 export default routes;
