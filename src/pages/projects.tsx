dd2dcd81dea2835f69c31a553d061d90fadacf22
John-Alexander Kamau <xander.kamau7@gmail.com>
2024-05-08 21:09:11 +0300

    adds pages for the home and projects

---
+++ src/pages/projects.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Projects: React.FC = () => {
+  return (
+    <div>
+      <h1>Projects Page</h1>
+      {/* Add your projects page content here */}
+    </div>
+  );
+};
+
+export default Projects;
