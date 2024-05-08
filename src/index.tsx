0cf9b6ab6f6a5c1becd0bf99900222d3360af791
John-Alexander Kamau <xander.kamau7@gmail.com>
2024-05-08 08:48:12 +0300

    imports and loads the environment variables using dotenv

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
+import dotenv from 'dotenv';
 import { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { StyleSheetManager } from "styled-components";
 import App from "./App";
 import "./index.css";
+dotenv.config();
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<Suspense fallback={<div>Loading...</div>}>
